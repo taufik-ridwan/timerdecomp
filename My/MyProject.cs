6792e32621cb753ea9e2a30668e6744408aecc37
Taufik R <75967358+taufik-ridwan@users.noreply.github.com>
2021-09-13 21:05:37 +0700

    Update MyProject.cs

--- My/MyProject.cs
+++ My/MyProject.cs
@@ -1,12 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: CountdownApp.My.MyProject
-// Assembly: CountdownApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: FC7E3B5E-A665-400F-94A5-48F1DBB775B0
-// Assembly location: C:\Users\Pika\Desktop\WaktuSimulasiAppv1.1.exe
-
-using Microsoft.VisualBasic;
-using Microsoft.VisualBasic.ApplicationServices;
-using Microsoft.VisualBasic.CompilerServices;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections;
@@ -17,232 +8,317 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace CountdownApp.My
 {
-  [StandardModule]
-  [HideModuleName]
-  [GeneratedCode("MyTemplate", "11.0.0.0")]
-  internal sealed class MyProject
-  {
-    private static readonly MyProject.ThreadSafeObjectProvider<MyComputer> m_ComputerObjectProvider = new MyProject.ThreadSafeObjectProvider<MyComputer>();
-    private static readonly MyProject.ThreadSafeObjectProvider<MyApplication> m_AppObjectProvider = new MyProject.ThreadSafeObjectProvider<MyApplication>();
-    private static readonly MyProject.ThreadSafeObjectProvider<User> m_UserObjectProvider = new MyProject.ThreadSafeObjectProvider<User>();
-    private static MyProject.ThreadSafeObjectProvider<MyProject.MyForms> m_MyFormsObjectProvider = new MyProject.ThreadSafeObjectProvider<MyProject.MyForms>();
-    private static readonly MyProject.ThreadSafeObjectProvider<MyProject.MyWebServices> m_MyWebServicesObjectProvider = new MyProject.ThreadSafeObjectProvider<MyProject.MyWebServices>();
+	[StandardModule]
+	[HideModuleName]
+	[GeneratedCode("MyTemplate", "11.0.0.0")]
+	internal sealed class MyProject
+	{
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		[MyGroupCollection("System.Windows.Forms.Form", "Create__Instance__", "Dispose__Instance__", "My.MyProject.Forms")]
+		internal sealed class MyForms
+		{
+			[ThreadStatic]
+			private static Hashtable m_FormBeingCreated;
 
-    [HelpKeyword("My.Computer")]
-    internal static MyComputer Computer
-    {
-      [DebuggerHidden] get => MyProject.m_ComputerObjectProvider.GetInstance;
-    }
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public frmCountdown m_frmCountdown;
 
-    [HelpKeyword("My.Application")]
-    internal static MyApplication Application
-    {
-      [DebuggerHidden] get => MyProject.m_AppObjectProvider.GetInstance;
-    }
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public frmMain m_frmMain;
 
-    [HelpKeyword("My.User")]
-    internal static User User
-    {
-      [DebuggerHidden] get => MyProject.m_UserObjectProvider.GetInstance;
-    }
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public frmStartup m_frmStartup;
 
-    [HelpKeyword("My.Forms")]
-    internal static MyProject.MyForms Forms
-    {
-      [DebuggerHidden] get => MyProject.m_MyFormsObjectProvider.GetInstance;
-    }
+			public frmCountdown frmCountdown
+			{
+				[DebuggerHidden]
+				get
+				{
+					m_frmCountdown = Create__Instance__(m_frmCountdown);
+					return m_frmCountdown;
+				}
+				[DebuggerHidden]
+				set
+				{
+					if (value != m_frmCountdown)
+					{
+						if (value != null)
+						{
+							throw new ArgumentException("Property can only be set to Nothing");
+						}
+						Dispose__Instance__(ref m_frmCountdown);
+					}
+				}
+			}
 
-    [HelpKeyword("My.WebServices")]
-    internal static MyProject.MyWebServices WebServices
-    {
-      [DebuggerHidden] get => MyProject.m_MyWebServicesObjectProvider.GetInstance;
-    }
+			public frmMain frmMain
+			{
+				[DebuggerHidden]
+				get
+				{
+					m_frmMain = Create__Instance__(m_frmMain);
+					return m_frmMain;
+				}
+				[DebuggerHidden]
+				set
+				{
+					if (value != m_frmMain)
+					{
+						if (value != null)
+						{
+							throw new ArgumentException("Property can only be set to Nothing");
+						}
+						Dispose__Instance__(ref m_frmMain);
+					}
+				}
+			}
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    [MyGroupCollection("System.Windows.Forms.Form", "Create__Instance__", "Dispose__Instance__", "My.MyProject.Forms")]
-    internal sealed class MyForms
-    {
-      [ThreadStatic]
-      private static Hashtable m_FormBeingCreated;
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public frmCountdown m_frmCountdown;
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public frmMain m_frmMain;
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public frmStartup m_frmStartup;
+			public frmStartup frmStartup
+			{
+				[DebuggerHidden]
+				get
+				{
+					m_frmStartup = Create__Instance__(m_frmStartup);
+					return m_frmStartup;
+				}
+				[DebuggerHidden]
+				set
+				{
+					if (value != m_frmStartup)
+					{
+						if (value != null)
+						{
+							throw new ArgumentException("Property can only be set to Nothing");
+						}
+						Dispose__Instance__(ref m_frmStartup);
+					}
+				}
+			}
 
-      [DebuggerHidden]
-      private static T Create__Instance__<T>(T Instance) where T : Form, new()
-      {
-        if ((object) Instance != null && !Instance.IsDisposed)
-          return Instance;
-        if (MyProject.MyForms.m_FormBeingCreated != null)
-        {
-          if (MyProject.MyForms.m_FormBeingCreated.ContainsKey((object) typeof (T)))
-            throw new InvalidOperationException(Utils.GetResourceString("WinForms_RecursiveFormCreate"));
-        }
-        else
-          MyProject.MyForms.m_FormBeingCreated = new Hashtable();
-        MyProject.MyForms.m_FormBeingCreated.Add((object) typeof (T), (object) null);
-        TargetInvocationException invocationException;
-        try
-        {
-          return new T();
-        }
-        catch (TargetInvocationException ex) when (
-        {
-          // ISSUE: unable to correctly present filter
-          ProjectData.SetProjectError((Exception) ex);
-          invocationException = ex;
-          if (invocationException.InnerException != null)
-          {
-            SuccessfulFiltering;
-          }
-          else
-            throw;
-        }
-        )
-        {
-          throw new InvalidOperationException(Utils.GetResourceString("WinForms_SeeInnerException", invocationException.InnerException.Message), invocationException.InnerException);
-        }
-        finally
-        {
-          MyProject.MyForms.m_FormBeingCreated.Remove((object) typeof (T));
-        }
-      }
+			[DebuggerHidden]
+			private static T Create__Instance__<T>(T Instance) where T : Form, new()
+			{
+				if (Instance?.IsDisposed ?? true)
+				{
+					if (m_FormBeingCreated != null)
+					{
+						if (m_FormBeingCreated.ContainsKey(typeof(T)))
+						{
+							throw new InvalidOperationException(Utils.GetResourceString("WinForms_RecursiveFormCreate"));
+						}
+					}
+					else
+					{
+						m_FormBeingCreated = new Hashtable();
+					}
+					m_FormBeingCreated.Add(typeof(T), null);
+					try
+					{
+						return new T();
+					}
+					catch (TargetInvocationException ex) when (((Func<bool>)delegate
+					{
+						// Could not convert BlockContainer to single expression
+						ProjectData.SetProjectError(ex);
+						return ex.InnerException != null;
+					}).Invoke())
+					{
+						string resourceString = Utils.GetResourceString("WinForms_SeeInnerException", ex.InnerException.Message);
+						throw new InvalidOperationException(resourceString, ex.InnerException);
+					}
+					finally
+					{
+						m_FormBeingCreated.Remove(typeof(T));
+					}
+				}
+				return Instance;
+			}
 
-      [DebuggerHidden]
-      private void Dispose__Instance__<T>(ref T instance) where T : Form
-      {
-        instance.Dispose();
-        instance = default (T);
-      }
+			[DebuggerHidden]
+			private void Dispose__Instance__<T>(ref T instance) where T : Form
+			{
+				instance.Dispose();
+				instance = null;
+			}
 
-      [DebuggerHidden]
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public MyForms()
-      {
-      }
+			[DebuggerHidden]
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public MyForms()
+			{
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public override bool Equals(object o) => base.Equals(RuntimeHelpers.GetObjectValue(o));
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public override bool Equals(object o)
+			{
+				return base.Equals(RuntimeHelpers.GetObjectValue(o));
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public override int GetHashCode() => base.GetHashCode();
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public override int GetHashCode()
+			{
+				return base.GetHashCode();
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      internal new System.Type GetType() => typeof (MyProject.MyForms);
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			internal new Type GetType()
+			{
+				return typeof(MyForms);
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public override string ToString() => base.ToString();
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public override string ToString()
+			{
+				return base.ToString();
+			}
+		}
 
-      public frmCountdown frmCountdown
-      {
-        [DebuggerHidden] get
-        {
-          this.m_frmCountdown = MyProject.MyForms.Create__Instance__<frmCountdown>(this.m_frmCountdown);
-          return this.m_frmCountdown;
-        }
-        [DebuggerHidden] set
-        {
-          if (value == this.m_frmCountdown)
-            return;
-          if (value != null)
-            throw new ArgumentException("Property can only be set to Nothing");
-          this.Dispose__Instance__<frmCountdown>(ref this.m_frmCountdown);
-        }
-      }
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		[MyGroupCollection("System.Web.Services.Protocols.SoapHttpClientProtocol", "Create__Instance__", "Dispose__Instance__", "")]
+		internal sealed class MyWebServices
+		{
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			[DebuggerHidden]
+			public override bool Equals(object o)
+			{
+				return base.Equals(RuntimeHelpers.GetObjectValue(o));
+			}
 
-      public frmMain frmMain
-      {
-        [DebuggerHidden] get
-        {
-          this.m_frmMain = MyProject.MyForms.Create__Instance__<frmMain>(this.m_frmMain);
-          return this.m_frmMain;
-        }
-        [DebuggerHidden] set
-        {
-          if (value == this.m_frmMain)
-            return;
-          if (value != null)
-            throw new ArgumentException("Property can only be set to Nothing");
-          this.Dispose__Instance__<frmMain>(ref this.m_frmMain);
-        }
-      }
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			[DebuggerHidden]
+			public override int GetHashCode()
+			{
+				return base.GetHashCode();
+			}
 
-      public frmStartup frmStartup
-      {
-        [DebuggerHidden] get
-        {
-          this.m_frmStartup = MyProject.MyForms.Create__Instance__<frmStartup>(this.m_frmStartup);
-          return this.m_frmStartup;
-        }
-        [DebuggerHidden] set
-        {
-          if (value == this.m_frmStartup)
-            return;
-          if (value != null)
-            throw new ArgumentException("Property can only be set to Nothing");
-          this.Dispose__Instance__<frmStartup>(ref this.m_frmStartup);
-        }
-      }
-    }
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			[DebuggerHidden]
+			internal new Type GetType()
+			{
+				return typeof(MyWebServices);
+			}
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    [MyGroupCollection("System.Web.Services.Protocols.SoapHttpClientProtocol", "Create__Instance__", "Dispose__Instance__", "")]
-    internal sealed class MyWebServices
-    {
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      [DebuggerHidden]
-      public override bool Equals(object o) => base.Equals(RuntimeHelpers.GetObjectValue(o));
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			[DebuggerHidden]
+			public override string ToString()
+			{
+				return base.ToString();
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      [DebuggerHidden]
-      public override int GetHashCode() => base.GetHashCode();
+			[DebuggerHidden]
+			private static T Create__Instance__<T>(T instance) where T : new()
+			{
+				if (instance == null)
+				{
+					return new T();
+				}
+				return instance;
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      [DebuggerHidden]
-      internal new System.Type GetType() => typeof (MyProject.MyWebServices);
+			[DebuggerHidden]
+			private void Dispose__Instance__<T>(ref T instance)
+			{
+				instance = default(T);
+			}
 
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      [DebuggerHidden]
-      public override string ToString() => base.ToString();
+			[DebuggerHidden]
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public MyWebServices()
+			{
+			}
+		}
 
-      [DebuggerHidden]
-      private static T Create__Instance__<T>(T instance) where T : new() => (object) instance == null ? new T() : instance;
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		[ComVisible(false)]
+		internal sealed class ThreadSafeObjectProvider<T> where T : new()
+		{
+			[CompilerGenerated]
+			[ThreadStatic]
+			private static T m_ThreadStaticValue;
 
-      [DebuggerHidden]
-      private void Dispose__Instance__<T>(ref T instance) => instance = default (T);
+			internal T GetInstance
+			{
+				[DebuggerHidden]
+				get
+				{
+					if (m_ThreadStaticValue == null)
+					{
+						m_ThreadStaticValue = new T();
+					}
+					return m_ThreadStaticValue;
+				}
+			}
 
-      [DebuggerHidden]
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public MyWebServices()
-      {
-      }
-    }
+			[DebuggerHidden]
+			[EditorBrowsable(EditorBrowsableState.Never)]
+			public ThreadSafeObjectProvider()
+			{
+			}
+		}
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    [ComVisible(false)]
-    internal sealed class ThreadSafeObjectProvider<T> where T : new()
-    {
-      internal T GetInstance
-      {
-        [DebuggerHidden] get
-        {
-          if ((object) MyProject.ThreadSafeObjectProvider<T>.m_ThreadStaticValue == null)
-            MyProject.ThreadSafeObjectProvider<T>.m_ThreadStaticValue = new T();
-          return MyProject.ThreadSafeObjectProvider<T>.m_ThreadStaticValue;
-        }
-      }
+		private static readonly ThreadSafeObjectProvider<MyComputer> m_ComputerObjectProvider = new ThreadSafeObjectProvider<MyComputer>();
 
-      [DebuggerHidden]
-      [EditorBrowsable(EditorBrowsableState.Never)]
-      public ThreadSafeObjectProvider()
-      {
-      }
-    }
-  }
+		private static readonly ThreadSafeObjectProvider<MyApplication> m_AppObjectProvider = new ThreadSafeObjectProvider<MyApplication>();
+
+		private static readonly ThreadSafeObjectProvider<User> m_UserObjectProvider = new ThreadSafeObjectProvider<User>();
+
+		private static ThreadSafeObjectProvider<MyForms> m_MyFormsObjectProvider = new ThreadSafeObjectProvider<MyForms>();
+
+		private static readonly ThreadSafeObjectProvider<MyWebServices> m_MyWebServicesObjectProvider = new ThreadSafeObjectProvider<MyWebServices>();
+
+		[HelpKeyword("My.Computer")]
+		internal static MyComputer Computer
+		{
+			[DebuggerHidden]
+			get
+			{
+				return m_ComputerObjectProvider.GetInstance;
+			}
+		}
+
+		[HelpKeyword("My.Application")]
+		internal static MyApplication Application
+		{
+			[DebuggerHidden]
+			get
+			{
+				return m_AppObjectProvider.GetInstance;
+			}
+		}
+
+		[HelpKeyword("My.User")]
+		internal static User User
+		{
+			[DebuggerHidden]
+			get
+			{
+				return m_UserObjectProvider.GetInstance;
+			}
+		}
+
+		[HelpKeyword("My.Forms")]
+		internal static MyForms Forms
+		{
+			[DebuggerHidden]
+			get
+			{
+				return m_MyFormsObjectProvider.GetInstance;
+			}
+		}
+
+		[HelpKeyword("My.WebServices")]
+		internal static MyWebServices WebServices
+		{
+			[DebuggerHidden]
+			get
+			{
+				return m_MyWebServicesObjectProvider.GetInstance;
+			}
+		}
+	}
 }
